d1db61eaf921f8a95e476d382768541f90255eac
nutkimsrun0967@gmail.com <nutkimsrun0967@gmail.com>
2024-04-25 18:41:55 +0700

    testing

--- Admint/html/authentication-register.php
+++ Admint/html/authentication-register.php
@@ -56,7 +56,6 @@
         </p>
       </div>
     </div>
-
   <script src="../assets/libs/jquery/dist/jquery.min.js"></script>
   <script src="../assets/libs/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
   <script>
